111abb7a339421589570c19716654722f6d10de9
Anandhakrishnan R K <67195364+anand1431@users.noreply.github.com>
2024-09-22 20:46:41 +0530

    Final update

--- main.c
+++ main.c
@@ -19,9 +19,46 @@ void GPIO_PORT_F_init(void)
     GPIO_PORTF_IEV_R = 0x00;
     GPIO_PORTF_IM_R  |= 0x11;
 }
+void systickInit(void)
+{
+    NVIC_ST_CURRENT_R = 0;
+    NVIC_ST_CTRL_R = 0x05;
+    NVIC_ST_RELOAD_R = 4000000;
+}
+
+void systick_timer()                           // DEFINING systick_timer FUNCTION
+{
+    systickInit();
+           NVIC_ST_RELOAD_R = 4000000;
+           while((NVIC_ST_CTRL_R & 0x10000)==0)
+                    {
+
+                    }
+           NVIC_ST_CTRL_R = 0x0;
+}
+
+void Portf_interrupt_handler(void)
+{
+    systick_timer();
+    if (GPIO_PORTF_RIS_R & 0x10)
+    {
+    GPIO_PORTF_DATA_R ^= 0x02;
+    GPIO_PORTF_ICR_R = 0x10;
+    }
+
+    if (GPIO_PORTF_RIS_R & 0x01)
+    {
+    GPIO_PORTF_DATA_R ^= 0x02;
+    GPIO_PORTF_ICR_R = 0x01;
+    }
+}
 
 
 int main(void)                               // MAIN FUNCTION
 {
-
+    GPIO_PORT_F_init();                      // GPIO INITIALISATION FUNCTION
+    systick_timer();                         // SYSTICK SETUP
+    while (1)
+    {                                        // DO NOTHING
+    }
 }
